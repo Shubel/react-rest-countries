e2c1e580d09d3be417116640c848cace15a47ed8
Sunny <shubelislam.diu@gmail.com>
2021-09-21 13:49:05 +0600

    footer added

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer><small>| Copyright | 2021 | COUNTRY REST API |</small></footer>
+        </div>
+    );
+};
+
+export default Footer;
