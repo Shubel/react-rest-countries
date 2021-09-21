c32b891b78570c515265ddc305e56a018da7de1e
Sunny <shubelislam.diu@gmail.com>
2021-09-21 07:27:47 +0600

    loaded countries and capital

--- src/App.js
+++ src/App.js
@@ -11,24 +11,31 @@ function App() {
 }
 
 function Countries() {
-
   const [countries, setCountries] = useState([]);
 
   useEffect(() => {
-
     fetch("https://restcountries.eu/rest/v2/all")
       .then(res => res.json())
       .then(data => setCountries(data));
-
   }, [])
 
   return (
-
     <div>
       <h2>Travelling around the globe !!</h2>
       <h3>total countries {countries.length}</h3>
+      {
+        countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+      }
     </div>
-    
+  );
+}
+
+function Country(props) {
+  return(
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h4>Capital: {props.capital}</h4>
+    </div>
   );
 }
 
