443759b916155af0ca0404626994213634826e21
Sunny <shubelislam.diu@gmail.com>
2021-09-21 13:23:55 +0600

    countries rest api deployed

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -8,16 +8,29 @@ const Countries = () => {
 
     useEffect(() => {
         fetch("https://restcountries.eu/rest/v2/all")
-        .then(res => res.json())
-        .then(data => setCountries(data));
+            .then(res => res.json())
+            .then(data => setCountries(data));
     },[])
 
     return (
         <div>
-            <h2 className="style">Country list: {countries.length}</h2>
+            <h2 className="style">Total Country List: {countries.length}</h2>
+
             {
-                countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+                countries.map(country => console.log(country))
             }
+
+            <div className="country-container">
+            {
+                countries.map(country => <Country 
+                    key={country.flag}
+                    country={country}
+                    // name={country.name} 
+                    // capital={country.capital} 
+                    // population={country.population}
+                    ></Country>)
+            }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './Country.css';
 
 const Country = (props) => {
-    // console.log(props)
+    const {name, capital, flag, population, region} = props.country;
+    // console.log(props.country)
+    
     return (
-        <div>
-            <h2>This is a {props.capital}</h2>
-            <h4>From {props.name}</h4>
+        <div className="country">
+            <img src={flag} alt="" />
+            <h3>Region: {region}</h3>
+            <h2>Country: {name}</h2>
+            <h4>Capital: {capital} and, population {population}</h4>
         </div>
     );
 };
